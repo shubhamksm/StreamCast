c96a5fdc147f2218aed97e25e7b3d120715fe1c9
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2020-06-12 01:11:56 +0530

    StreamEdit component changed to add all functionality, and EditStream action's rest method updated

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,33 @@
 import React from "react";
+import _ from "lodash";
 import { connect } from "react-redux";
 
-import { fetchStream } from "../../actions";
+import { fetchStream, editSteam } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+    this.props.editSteam(this.props.stream.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>loading</div>;
     } else {
-      return <div>{this.props.stream.title}</div>;
+      return (
+        <div>
+          <h3>Edit a Stream</h3>
+          <StreamForm
+            initialValues={_.pick(this.props.stream, "title", "description")}
+            onSubmit={this.onSubmit}
+          />
+        </div>
+      );
     }
   }
 }
@@ -21,4 +36,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editSteam })(StreamEdit);
